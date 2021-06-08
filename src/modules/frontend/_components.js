3d1a6a15ebe568b588e57ab15ab4ba38cb634945
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-06-08 18:11:35 -0400

    fix lintern errors

--- src/modules/frontend/_components.js
+++ src/modules/frontend/_components.js
@@ -1,17 +1,7 @@
-import { fetchWeather } from "../backend/_api"
-
-const formSection = () => {
-  const section = document.createElement('section');
-  section.id = 'form_section';
-  section.classList.add('secondary');
-
-  section.appendChild(formForm());
-
-  return section;
-};
+import fetchWeather from '../backend/_api';
 
 const formForm = () => {
-  const form = document.createElement('form')
+  const form = document.createElement('form');
   const h1 = document.createElement('h1');
   h1.textContent = 'Weather';
   form.appendChild(h1);
@@ -24,12 +14,48 @@ const formForm = () => {
   submitButton.textContent = 'submit';
   submitButton.type = 'button';
   submitButton.classList.add('primary');
-  submitButton.addEventListener('click', () => 
-    fetchWeather(inputText.value));
+  submitButton.addEventListener('click', () => fetchWeather(inputText.value));
   form.appendChild(submitButton);
 
   return form;
-}
+};
+
+const formSection = () => {
+  const section = document.createElement('section');
+  section.id = 'form_section';
+  section.classList.add('secondary');
+
+  section.appendChild(formForm());
+
+  return section;
+};
+
+const resultsList = (cityName, temperature, picture, otherData) => {
+  const article = document.createElement('article');
+
+  const icon = document.createElement('img');
+  icon.src = `http://openweathermap.org/img/wn/${picture}@2x.png`;
+  article.appendChild(icon);
+
+  const h1 = document.createElement('h1');
+  h1.textContent = cityName;
+  article.appendChild(h1);
+
+  const h3 = document.createElement('h3');
+  h3.textContent = `${temperature} \u2103 `;
+  article.appendChild(h3);
+
+  otherData.map((data) => {
+    const p = document.createElement('p');
+    p.textContent = `${data[0]} ${data[1]} ${data[2]}`;
+
+    article.appendChild(p);
+
+    return p;
+  });
+
+  return article;
+};
 
 const resultsSection = (jsonObject = null) => {
   const section = document.createElement('section');
@@ -51,30 +77,5 @@ const resultsSection = (jsonObject = null) => {
 
   return section;
 };
-
-const resultsList = (cityName, temperature, picture, otherData) => {
-  const article = document.createElement('article');
-
-  const icon = document.createElement('img');
-  icon.src = `http://openweathermap.org/img/wn/${picture}@2x.png`
-  article.appendChild(icon);
-
-  const h1 = document.createElement('h1');
-  h1.textContent = cityName;
-  article.appendChild(h1);
-
-  const h3 = document.createElement('h3');
-  h3.textContent = `${temperature} \u2103 `;
-  article.appendChild(h3);
-
-  otherData?.map(data => {
-    const p = document.createElement('p');
-    p.textContent = `${data[0]} ${data[1]} ${data[2]}`;
-
-    article.appendChild(p);
-  });
-
-  return article;
-}
 
 export { formSection, resultsSection };
