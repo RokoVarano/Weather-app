947ffbc5b4cbfad8fdab4bd31a9139f3c5b9d9a9
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-06-08 18:20:20 -0400

    fix https problem for netlify

--- src/modules/backend/_api.js
+++ src/modules/backend/_api.js
@@ -1,7 +1,7 @@
 const fetchWeather = async (cityName) => {
   const key = 'b48b5880b18acb3e39d1078bbf46d6c2';
 
-  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${key}`);
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${key}`);
 
   const json = await response.text();
 
